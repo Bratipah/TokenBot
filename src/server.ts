f6ae64b9c166eb8bdec6264d756a9a0ba42d6a01
Bratipah <bratipahmh@gmail.com>
2024-07-24 23:38:06 +0300

    update code

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+// server.ts
+import 'dotenv/config';
+import express, { Request, Response, NextFunction, Express } from 'express';
+import { bot } from './bot'; // Ensure this path matches the location of your bot script
+
+
+const app: Express = express();
+const port = process.env.PORT || 3000; // Handles the port setting for Railway or other platforms
+const token = process.env.TELEGRAM_BOT_TOKEN;
+
+// Properly configure bodyParser to parse JSON payloads
+app.use(express.json());
+
+// Handle updates from Telegram
+app.post(`/bot${token}`, (req: Request, res: Response) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
+
+// Optional: GET route for the root to verify the server is running
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello from Telegram Bot Server!');
+});
+
+// Error handling middleware for handling Express errors
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start Express server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// export { app }; // Exporting app for testing purposes
---
+++ src/types.d.ts
@@ -0,0 +1,18 @@
+// src/types.d.ts
+
+declare module 'telegram-inline-calendar' {
+    import TelegramBot from 'node-telegram-bot-api';
+
+    export interface CalendarOptions {
+        date_format: string;
+        language: string;
+        bot_api?: 'telebot' | 'node-telegram-bot-api';
+    }
+
+    export class Calendar {
+        [x: string]: any;
+        constructor(bot: TelegramBot, options: CalendarOptions);
+        startNavCalendar(msg: any): void;
+        clickButtonCalendar(query: any): string | -1;
+    }
+}
